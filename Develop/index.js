eb0b46fa9d8c8134fa04ece2e3ce2d9dc69bfdbe
unknown <ramondokenny585@gmail.com>
2023-07-27 11:07:42 -0400

    gitignore commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    message: "What is the title of you project?",
+    name: "Title",
+  },
+  {
+    message: "What is your project?",
+    name: "Description",
+  },
+  {
+    message: "What problem does it solve?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
